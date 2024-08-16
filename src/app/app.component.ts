cd156c28552b2a3cda719cd3bcccdd8875efdf07
JrivasCodes <jrivas.codes@gmail.com>
2024-08-16 15:16:57 -0700

    added Map component and added the SVG Map as well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { MapComponent } from "./components/map/map.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MapComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-
+import { MapComponent } from './components/map/map.component';
 export const routes: Routes = [
     {
         path: 'header',
         component: HeaderComponent,
     },
+    {
+        path: 'map',
+        component: MapComponent,
+    }
 ];
