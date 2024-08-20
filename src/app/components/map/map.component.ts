3cf9a73671e1e2d777a49ef3ae6694cdd601f62a
JrivasCodes <jrivas.codes@gmail.com>
2024-08-20 16:55:30 -0700

    working on making the countries to highlight when hovering

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, output } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -8,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './map.component.css'
 })
 export class MapComponent {
+  @Output() regionSelected = new EventEmitter<{ name: string; id: string }>();
+// private selectedElement: HTMLElement | null = null;
 
+onRegionMouseEnter(event: MouseEvent) {
+  const target = event.target as SVGPathElement;
+
+  if (target.tagName === 'path') {
+    console.log('Mouse Over Path:', target);
+    target.classList.add('regionHover');
+  }
+}
+
+  onRegionMouseLeave(event: MouseEvent){
+    const target = event.target as HTMLElement;
+    console.log('Mouse Left:', target);
+    if(target.tagName === 'path'){
+      target.classList.remove('regionHover')
+    }
+  }
+
+  onSvgClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+
+
+    if (target.tagName === 'path'){
+      const regionName = target.getAttribute('name');
+      const regionId = target.id;
+      console.log('region clicked', regionName, regionId)
+    }
+  }
 }
