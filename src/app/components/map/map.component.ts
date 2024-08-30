021fbfc74d735e86b1294e355ef5e03cb14cb1ee
JrivasCodes <jrivas.codes@gmail.com>
2024-08-30 15:44:18 -0700

    Got the Api to work with the site, and made it display the info correctly

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,21 @@
 import { Component, ElementRef, AfterViewInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CountryDataService } from '../country-data.service'; // this is from the api service component 
+import { importProvidersFrom } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
 export class MapComponent implements AfterViewInit {
 
-  constructor(private elementRef: ElementRef) {}
+
+
+  constructor(private elementRef: ElementRef, private countryDataService: CountryDataService) {}
+  countryData: any;
   selectedPath: SVGPathElement | null = null;
 
 
@@ -25,14 +31,14 @@ export class MapComponent implements AfterViewInit {
   // This is the function for when the mouse enters a country hover effect
   onRegionMouseEnter(event: MouseEvent) {
     const target = event.target as SVGPathElement;
-    console.log('Mouse Entered Path:', target);
+    // console.log('Mouse Entered Path:', target);
     target.classList.add('regionHover');
   }
 
   // This is the function for when the mouse leaves a country hover effect
   onRegionMouseLeave(event: MouseEvent) {
     const target = event.target as SVGPathElement;
-    console.log('Mouse Left Path:', target);
+    // console.log('Mouse Left Path:', target);
     target.classList.remove('regionHover');
   }
 
@@ -52,6 +58,11 @@ export class MapComponent implements AfterViewInit {
       const regionName = target.getAttribute('name');
       const regionId = target.id;
       console.log('region clicked', regionName, regionId)
+
+      this.countryDataService.getCountryData(regionId).subscribe(data => {
+        this.countryData = data[1][0];
+        console.log('Country Data:', this.countryData)
+      })
     }
   }
 }
